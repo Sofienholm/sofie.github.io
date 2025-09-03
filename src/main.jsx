e8fe325489246268c77614c2041667d12de6459f
Sofienholm <eaa24snho@students.eaaa.dk>
2025-09-03 10:30:17 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter
-      basename={import.meta.env.DEV ? "/" : "/YOUR-REPOSITORY-NAME/"}
-    >
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/sofie.github.io/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
