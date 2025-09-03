2bb523603544e0a8be7cd8c1b7f5ae6f0d61def4
Sofienholm <eaa24snho@students.eaaa.dk>
2025-09-03 10:20:16 +0200

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
-import Links from "./pages/Links";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import Nav from "./Components/NavLink";
 
 function App() {
   return (
-    <Routes>
-      {/* Routes go here */}
-      <Route path="/" element={<Home />} />
-      <Route path="/links" element={<Links />} />
-    </Routes>
+    <div>
+      <Nav />
+      <Routes>
+        {/* Routes go here */}
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Projects" element={<Projects />} />
+      </Routes>
+    </div>
   );
 }
 
